8cba0bb4b11ba023e3e31b980b534c005552f901
Liyi <liyi4x@gmail.com>
2021-09-06 16:37:59 +0800

    添加group，只差/move_base_simple/goal话题

--- roborts_decision/behavior_test.cpp
+++ roborts_decision/behavior_test.cpp
@@ -19,52 +19,61 @@ int main(int argc, char **argv) {
   auto chassis_executor = new roborts_decision::ChassisExecutor;
   auto blackboard = new roborts_decision::Blackboard(full_path);
 
-  roborts_decision::BackBootAreaBehavior back_boot_area_behavior(chassis_executor, blackboard, full_path);
+  // roborts_decision::BackBootAreaBehavior back_boot_area_behavior(chassis_executor, blackboard, full_path);
   // roborts_decision::ChaseBehavior        chase_behavior(chassis_executor, blackboard, full_path);
   // roborts_decision::SearchBehavior       search_behavior(chassis_executor, blackboard, full_path);
   // roborts_decision::EscapeBehavior       escape_behavior(chassis_executor, blackboard, full_path);
   // roborts_decision::PatrolBehavior       patrol_behavior(chassis_executor, blackboard, full_path);
   roborts_decision::GoalBehavior       goal_behavior(chassis_executor, blackboard);
-
-  auto command_thread= std::thread(Command);
+  
+  
   ros::Rate rate(10);
-  while(ros::ok()){
+  while(ros::ok())
+  {
     ros::spinOnce();
-    switch (command) {
-      //back to boot area
-      case '1':
-        back_boot_area_behavior.Run();
-        break;
-      //   //patrol
-      // case '2':
-      //   patrol_behavior.Run();
-      //   break;
-      //   //chase.
-      // case '3':
-      //   chase_behavior.Run();
-      //   break;
-      //   //search
-      // case '4':
-      //   search_behavior.Run();
-      //   break;
-      //   //escape.
-      // case '5':
-      //   escape_behavior.Run();
-      //   break;
-        //goal.
-      case '6':
-        goal_behavior.Run();
-        break;
-      case 27:
-        if (command_thread.joinable()){
-          command_thread.join();
-        }
-        return 0;
-      default:
-        break;
-    }
+    goal_behavior.Run();
     rate.sleep();
   }
+
+  // auto command_thread= std::thread(Command);
+  // ros::Rate rate(10);
+  // while(ros::ok()){
+  //   ros::spinOnce();
+  //   switch (command) {
+  //     // //back to boot area
+  //     // case '1':
+  //     //   back_boot_area_behavior.Run();
+  //     //   break;
+  //     //   //patrol
+  //     // case '2':
+  //     //   patrol_behavior.Run();
+  //     //   break;
+  //     //   //chase.
+  //     // case '3':
+  //     //   chase_behavior.Run();
+  //     //   break;
+  //     //   //search
+  //     // case '4':
+  //     //   search_behavior.Run();
+  //     //   break;
+  //     //   //escape.
+  //     // case '5':
+  //     //   escape_behavior.Run();
+  //     //   break;
+  //       //goal.
+  //     case '6':
+  //       goal_behavior.Run();
+  //       break;
+  //     case 27:
+  //       if (command_thread.joinable()){
+  //         command_thread.join();
+  //       }
+  //       return 0;
+  //     default:
+  //       break;
+  //   }
+  //   rate.sleep();
+  // }
 
 
   return 0;
